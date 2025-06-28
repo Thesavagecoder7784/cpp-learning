bf65011a633e97b0b2789f295d57eaf7e096217f
Thesavagecoder7784 <prabhatmaster2005@gmail.com>
2025-06-28 19:01:40 +0530

    Update learning.cpp

--- learning.cpp
+++ learning.cpp
@@ -34,10 +34,10 @@ int main() {
 
   cout << 15%7; // output = 1
 
-  int i,j;
-  i = j = 10;
-  i++;
-  j++;
+  int n,m;
+  n = m = 10;
+  n++;
+  m++;
   
 	x %= 3;  // x = x % 3
   x &= 3;  // x = x & 3 - AND operation
@@ -51,5 +51,99 @@ int main() {
   cout << x && y; // Logical AND
   cout << x || y; // Logical OR
   cout << "\n";
+
+  if (20 > 18) {
+  cout << "20 is greater than 18";
+  }
+  cout << "\n";
+
+  int time = 22;
+  if (time < 10) {
+    cout << "Good morning.";
+  } else if (time < 20) {
+    cout << "Good day.";
+  } else {
+    cout << "Good evening.";
+  }
+  cout << "\n";
+
+  int day = 4;
+  switch (day) {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Saturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+  }
+
+  int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
+
+  int j = 0;
+  do {
+    cout << j << "\n";
+    j++;
+  }
+  while (j < 5);
+
+
+  for (int k = 0; k < 5; k++) {
+  cout << k << "\n";
+  }
+
+  for (i = 1; i <= 3; i++) {
+  for (j = 1; j <= 3; j++) {
+      cout << i * j << " ";
+    }
+    cout << "\n";
+  }
+
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  for (int a : myNumbers) {
+    cout << a << "\n";
+  }
+
+  i = 0;
+  while (i < 10) {
+    cout << i << "\n";
+    i++;
+    if (i == 4) {
+      break;
+    }
+  }
+
+  i = 0;
+  while (i < 10) {
+    if (i == 4) {
+      i++;
+      continue;
+    }
+    cout << i << "\n";
+    i++;
+  }
+
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+  cout << cars[0];
+  cout << "/n";
+
   return 0;
 }
