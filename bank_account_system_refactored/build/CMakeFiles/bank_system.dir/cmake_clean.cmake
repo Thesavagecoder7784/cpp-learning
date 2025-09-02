f358e0ebb8a7a03594656d4b4beb598436f432e8
Thesavagecoder7784 <prabhatmaster2005@gmail.com>
2025-09-01 22:40:45 -0400

    Build Cmake

---
+++ bank_account_system_refactored/build/CMakeFiles/bank_system.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_system.dir/src/BankAccount.cpp.o"
+  "CMakeFiles/bank_system.dir/src/BankAccount.cpp.o.d"
+  "CMakeFiles/bank_system.dir/src/main.cpp.o"
+  "CMakeFiles/bank_system.dir/src/main.cpp.o.d"
+  "CMakeFiles/bank_system.dir/src/utils.cpp.o"
+  "CMakeFiles/bank_system.dir/src/utils.cpp.o.d"
+  "bank_system"
+  "bank_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
