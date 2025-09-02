6abf8fa77912fec78af9d7fbb0e3a36e8bfc6066
Thesavagecoder7784 <prabhatmaster2005@gmail.com>
2025-09-01 22:32:38 -0400

    Create utils.h

---
+++ bank_account_system_refactored/src/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+int getIntegerInput(const std::string& prompt);
+double getDoubleInput(const std::string& prompt);
+std::string getStringInput(const std::string& prompt);
+
+#endif // UTILS_H
